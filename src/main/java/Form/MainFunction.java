39a1938f723f8fed46389e91ba7687c44fee6261
kenjyh301 <kenjyh301@gmail.com>
2021-06-13 23:22:29 +0700

    create basic form

--- src/main/java/Form/MainFunction.java
+++ src/main/java/Form/MainFunction.java
@@ -1,4 +1,13 @@
 package Form;
 
+import javax.swing.*;
+
 public class MainFunction {
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
